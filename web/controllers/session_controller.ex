0ee30bbb73c05bc3b6e1c659fd48fada35751e5b
chansuke <moonset20@gmail.com>
2016-05-26 00:00:34 +0900

    Add create controller

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,7 +1,32 @@
 defmodule Chimera.SessionController do
   use Chimera.Web, :controller
+  alias Chimera.User
+
+  import Comeonin.Bcrypt, only: [checkpw: 2]
+
+  plug :scrub_params, "User" when action in [:create]
 
   def new(conn, _params) do
-    render conn, "new.html"
+    render conn, "new.html", changeset: User.changeset(%User{})
+  end
+
+  defp sign_in(user, password, conn) when is_nil(user) do
+    conn
+    |> put_flash(:error, "Invalid username/password combination!")
+    |> redirect(to: page_path(conn, :index))
+  end
+
+  defp sign_in(user, password, conn) do
+    if checkpw(password, user.password_digest) do
+      conn
+      |> put_session(:current_user, %{id: user.id, username: user.username})
+      |> put_flash(:info, "Sign in succesful!")
+      |> redirect(to: page_path(conn, :index))
+    else
+      conn
+      |> put_session(:current_user, nil)
+      |> put_flash(:error, "Invalid username/password combination!")
+      |> redirect(to: page_path(conn, :index))
+    end
   end
 end
