fe745c0d61298cd11715d1fc90b617b0262a8755
chansuke <moonset20@gmail.com>
2016-06-02 04:39:46 +0900

    Fix bug

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,18 +1,21 @@
 defmodule Chimera.SessionController do
   use Chimera.Web, :controller
   alias Chimera.User
-
   import Comeonin.Bcrypt, only: [checkpw: 2]
 
-  plug :scrub_params, "User" when action in [:create]
+  plug :scrub_params, "user" when action in [:create]
 
   def new(conn, _params) do
     render conn, "new.html", changeset: User.changeset(%User{})
   end
 
-  def create(conn, %{"user" => user_params}) do
-    Repo.get_by(User, username: user_params["username"])
-    |> sign_in(user_params["password"], conn)
+  def create(conn, %{"user" => %{"username" => username, "password" => password}}) when not is_nil(username) and not is_nil(password) do
+    user = Repo.get_by(User, username: username)
+    sign_in(user, password, conn)
+  end
+
+  def create(conn, _) do
+    failed_login(conn)
   end
 
   def delete(conn, _params) do
@@ -23,9 +26,7 @@ defmodule Chimera.SessionController do
   end
 
   defp sign_in(user, password, conn) when is_nil(user) do
-    conn
-    |> put_flash(:error, "Invalid username/password combination!")
-    |> redirect(to: page_path(conn, :index))
+    failed_login(conn)
   end
 
   defp sign_in(user, password, conn) do
@@ -35,10 +36,15 @@ defmodule Chimera.SessionController do
       |> put_flash(:info, "Sign in succesful!")
       |> redirect(to: page_path(conn, :index))
     else
-      conn
-      |> put_session(:current_user, nil)
-      |> put_flash(:error, "Invalid username/password combination!")
-      |> redirect(to: page_path(conn, :index))
+      failed_login(conn)
     end
+  end
+
+  defp failed_login(conn) do
+    conn
+    |> put_session(:current_user, nil)
+    |> put_flash(:error, "Invalid username/password combination!")
+    |> redirect(to: page_path(conn, :index))
+    |> halt()
   end
 end
--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,8 +1,7 @@
 defmodule Chimera.LayoutView do
   use Chimera.Web, :view
-end
 
-def current_user(conn) do
-  Plug.Conn.get_session(conn, :current_user)
+  def current_user(conn) do
+    Plug.Conn.get_session(conn, :current_user)
+  end
 end
-
